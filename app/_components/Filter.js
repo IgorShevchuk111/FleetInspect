d7a9405a1e25afefa275f7051adf0fd8aca4d068
Igor Shevchuk <igorshevchuk111@gmail.com>
2025-01-22 22:02:25 +0000

    Create Filter for inspectionList

---
+++ app/_components/Filter.js
@@ -0,0 +1,51 @@
+'use client';
+
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+
+const options = [
+  { value: 'all', label: ' All' },
+  { value: 'truck', label: 'Trucks' },
+  { value: 'trailer', label: 'Trailers' },
+];
+
+function Filter() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathName = usePathname();
+  const activeFilter = searchParams.get('filter') ?? 'all';
+
+  function handleFilter(filter) {
+    const params = new URLSearchParams(searchParams);
+    params.set('filter', filter);
+    router.replace(`${pathName}?${params.toString()}`, { scroll: false });
+  }
+  return (
+    <div className=" flex items-center">
+      {options?.map((option) => (
+        <Button
+          key={option.value}
+          activeFilter={activeFilter}
+          handleFilter={handleFilter}
+          filter={option.value}
+        >
+          {option.label}
+        </Button>
+      ))}
+    </div>
+  );
+}
+
+function Button({ filter, handleFilter, activeFilter, children }) {
+  return (
+    <button
+      onClick={() => handleFilter(filter)}
+      className={`
+      ${activeFilter === filter ? 'border rounded-md border-gray-500' : ''} 
+      px-2 py-1 text-gray-700`}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Filter;
