1788ae6bd6ee919c796f9628cc911f7fe27a56ad
Igor Shevchuk <igorshevchuk111@gmail.com>
2025-01-30 13:38:26 +0000

    Refactoring

--- app/inspection/thankyou/page.js
+++ app/inspection/thankyou/page.js
@@ -3,11 +3,11 @@ import Link from 'next/link';
 
 export default function Page() {
   return (
-    <div className="text-center space-y-6 mt-4">
+    <div className="text-center space-y-6 px-4 py-6">
       <h1 className="text-3xl font-semibold">Thank you for your inspection!</h1>
       <div className=" flex gap-2 justify-center">
-        <ButtonLink label="Go to inspection page" href="/inspection" />
-        <ButtonLink label="Go to my inspections" href="/inspections" />
+        <ButtonLink label="Inspection page" href="/inspection" />
+        <ButtonLink label="My inspections" href="/inspections" />
       </div>
     </div>
   );
