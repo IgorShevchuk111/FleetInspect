f9b943a7e59767bce89170b1371c7f3a92bb2de2
Igor Shevchuk <igorshevchuk111@gmail.com>
2025-02-11 21:58:08 +0000

    Refactoring

--- app/_components/FormFieldImageUpload.js
+++ app/_components/FormFieldImageUpload.js
@@ -15,6 +15,7 @@ const FormFieldImageUpload = ({
   setCompressedImages,
   error,
   clearErrors,
+  setValue,
 }) => {
   const [preview, setPreview] = useState('');
 
@@ -32,6 +33,7 @@ const FormFieldImageUpload = ({
       lastModified: Date.now(),
     });
 
+    setValue(name, compressedFile);
     setPreview(URL.createObjectURL(compressedFile));
     setCompressedImages((prevImages) => ({
       ...prevImages,
